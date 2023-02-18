79b10743ed6d406921d80ca0ccdb57b21645d49e
Irina <pimaki2012@mail.ru>
2023-02-18 19:22:00 +0300

    задача 13

--- task13/Program.cs
+++ task13/Program.cs
@@ -10,10 +10,3 @@ if ( int.Parse(a) < 99) {
     Console.WriteLine("Третьей цифры нет, поробуйте ввсести другое число");
 } else
     Console.WriteLine($"{a} -> {a[2]}");
-
-
-// погуглить что это и разобраться 
- Console.WriteLine("пробел");
-int n = int.Parse(Console.ReadLine());
-Console.WriteLine(n>99 ? n.ToString()[2] : '-');
- Console.ReadKey(true);
